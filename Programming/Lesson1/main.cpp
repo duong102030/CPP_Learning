402f8c87e8558067db4d6ccce13beede75bd516a
duong102030 <nguyenkhaduong1010@gmail.com>
2026-04-22 00:36:54 +0700

    Add take note for lesson 1

--- Programming/Lesson1/main.cpp
+++ Programming/Lesson1/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+/* 
+ * So nguyen:
+ * int: 4 byte = 32 bit : -2^31 -> 2^31 - 1 
+ * long long: 8 byte = 64 bit: -2^63 -> 2^63 
+ * 
+ * So thuc:
+ * float: 4 byte = 32 bit : 6 so thap phan
+ * double: 8 byte = 64 bit : 15 so thap phan
+ * 
+ * Ky tu:
+ * char: character = 1 byte
+
+*/
 int main() {
     cout << "Hello CodeLite!" << endl;
     cin.get();
